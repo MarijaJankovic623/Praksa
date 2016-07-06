4d8a52d5d0f848dd42445ae9454bbf4394bde2e5
Marija <marijajankovic623@gmail.com>
2016-07-06 13:09:37 +0200

    Stavljen skript za kreiranje baze

--- baza/create_script.sql
+++ baza/create_script.sql
@@ -2,6 +2,7 @@ CREATE TABLE `korisnik` (
   `id_korisnik` int(11) NOT NULL,
   `korisnicko_ime` varchar(100) NOT NULL,
   `lozinka` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
   PRIMARY KEY (`id_korisnik`),
   UNIQUE KEY `korisnicko_ime_UNIQUE` (`korisnicko_ime`),
   UNIQUE KEY `lozinka_UNIQUE` (`lozinka`)
